5b2b7662eb7aac6af12d13e91ba663523d651989
Ali-Elchab <alielchab01@gmail.com>
2023-12-24 17:49:20 +0200

    Backend: Added users, social media, and job seekers migrations

--- server/database/migrations/2023_12_24_152511_create_social_media_links_table.php
+++ server/database/migrations/2023_12_24_152511_create_social_media_links_table.php
@@ -11,14 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('social_media_links', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('platform');
+            $table->string('link');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
         });
     }
 
@@ -27,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('social_media_links');
     }
 };
---
+++ server/database/migrations/2023_12_24_152749_create_job_seekers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('job_seekers', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('profile_pic');
+            $table->date('dob');
+            $table->string('phone');
+            $table->string('address');
+            $table->string('city');
+            $table->string('bio');
+            $table->string('resume');
+            $table->string('cover_letter');
+            $table->boolean('is_available');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('job_seekers');
+    }
+};
