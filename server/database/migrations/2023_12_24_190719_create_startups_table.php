88861e48a09b962b087c9cf76b24f397e078f437
Ali-Elchab <alielchab01@gmail.com>
2023-12-24 21:20:00 +0200

    Backend: Added startup and industries migrations

---
+++ server/database/migrations/2023_12_24_190719_create_startups_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('startups', function (Blueprint $table) {
+            $table->id();
+            $table->string('company_name');
+            $table->string('company_email')->unique();
+            $table->string('company_phone');
+            $table->string('profile_pic');
+            $table->string('registeration_number');
+            $table->date('founding_date');
+            $table->string('company_address');
+            $table->string('website_url');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('industry_id')->constrained()->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('startups');
+    }
+};
