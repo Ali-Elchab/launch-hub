555b803e5d81114f50c965ca33f82612516ae442
Ali-Elchab <alielchab01@gmail.com>
2023-12-24 21:48:11 +0200

    Backend: Added job_seeker_enhace_skills migration

---
+++ server/database/migrations/2023_12_24_194624_create_job_seeker_enhace_skills_courses_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('job_seeker_enhace_skills_courses', function (Blueprint $table) {
+            $table->id();
+            $table->string('course_name');
+            $table->string('link');
+            $table->string('image_url');
+            $table->string('description');
+            $table->foreignId('job_seeker_id')->constrained()->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('job_seeker_enhace_skills_courses');
+    }
+};
