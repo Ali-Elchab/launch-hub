591be19ebd7430c867deb742d7061989cd9e45a6
Ali-Elchab <alielchab01@gmail.com>
2023-12-25 10:48:12 +0200

    Backend: Added certification and experience  models

---
+++ server/app/Models/Certification.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Certification extends Model
+{
+    use HasFactory;
+
+
+    protected $fillable = [
+        'name',
+        'certificate',
+        'organization',
+        'start_date',
+        'end_date',
+        'description',
+        'location',
+        'job_seeker_id',
+    ];
+
+    public function jobSeeker()
+    {
+        return $this->belongsTo(JobSeeker::class);
+    }
+}
