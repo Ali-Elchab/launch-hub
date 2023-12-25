e03be986abeb86bb07196d597bfb532f13d1a77b
Ali-Elchab <alielchab01@gmail.com>
2023-12-25 11:33:53 +0200

    Backend: Added JobPost model

---
+++ server/app/Models/JobPost.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class JobPost extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'job_title',
+        'job_description',
+        'industry',
+        'job_location',
+        'job_type',
+        'job_salary',
+        'job_qualification',
+        'experience_level',
+        'education_level',
+        'preferred_gender',
+        'deadline',
+        'job_status',
+        'industry_id',
+        'startup_id',
+    ];
+
+    public function industry()
+    {
+        return $this->belongsTo(Industry::class);
+    }
+
+    public function startup()
+    {
+        return $this->belongsTo(Startup::class);
+    }
+
+    public function applications()
+    {
+        return $this->hasMany(Application::class);
+    }
+
+    public function requiredSkills()
+    {
+        return $this->belongsToMany(Skill::class, 'job_post_skills', 'job_post_id', 'skill_id');
+    }
+}
