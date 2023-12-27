6ea4af6ac0ff727a9dcde63a4e55dfa287355b9c
Ali-Elchab <alielchab01@gmail.com>
2023-12-27 14:52:08 +0200

    Backend: Added Job seeker api endpoints

--- server/app/Http/Controllers/UserController.php
+++ server/app/Http/Controllers/UserController.php
@@ -8,8 +8,37 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class UserController extends Controller
 {
     use HasFactory, Notifiable;
+
+
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
+
+
+    public function deleteJobSeekerProfile(Request $request)
+    {
+        $user = $request->user();
+        $jobSeeker = $user->jobSeeker;
+
+        if ($jobSeeker) {
+
+            $resume = $jobSeeker->resume;
+            $resumePath = 'assets/resumes/' . $resume;
+
+            if ($resume && Storage::disk('public')->exists($resumePath)) {
+                Storage::disk('public')->delete($resumePath);
+            }
+            $jobSeeker->delete();
+
+            return response()->json(['status' => 'success', 'message' => 'Job seeker and resume deleted successfully']);
+        }
+
+        return response()->json(['status' => 'error', 'message' => 'Job seeker not found'], 404);
+    }
 }
