e536b4e40fa2127aafed107acbe01496002daa8a
Ali-Elchab <alielchab01@gmail.com>
2023-12-27 14:51:57 +0200

    Backend: Added Job seeker api endpoints

--- server/app/Http/Controllers/JobSeekerController.php
+++ server/app/Http/Controllers/JobSeekerController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\JobPost;
 use App\Models\Skill;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class JobSeekerController extends Controller
 {
@@ -14,6 +16,19 @@ class JobSeekerController extends Controller
         $this->middleware('auth:api');
     }
 
+    public function getJobSeekerProfile(Request $request)
+    {
+        $user = $request->user();
+
+        if ($user && $user->jobSeeker) {
+            $jobSeeker = $user->jobSeeker->load(['courses', 'educations', 'experiences', 'certifications', 'hobbies', 'industry', 'skills', 'jobPosts']);
+            $jobSeeker->socialMediaLinks = $user->socialMediaLinks;
+
+            return response()->json(['status' => 'success', 'jobSeeker' => $jobSeeker]);
+        }
+
+        return response()->json(['status' => 'error', 'message' => 'Job seeker not found'], 404);
+    }
 
     public function getJobSeekerskills(Request $request)
     {
@@ -27,17 +42,45 @@ class JobSeekerController extends Controller
         return response()->json(['status' => 'error', 'message' => 'Job seeker not found'], 404);
     }
 
-    public function getJobSeekerProfile(Request $request)
+    public function updateJobSeekerProfile(Request $request)
     {
-        $user = $request->user();
+        $profileImagePath = uploadImage($request);
 
-        if ($user && $user->jobSeeker) {
-            $jobSeeker = $user->jobSeeker->load(['courses', 'educations', 'experiences', 'certifications', 'hobbies', 'industry', 'skills']);
-            $jobSeeker->socialMediaLinks = $user->socialMediaLinks;
+        $jobSeeker = $request->user()->jobSeeker;
+        if ($jobSeeker) {
+            $updateData = $request->only('first_name', 'last_name', 'phone', 'address', 'city', 'bio');
+            if ($profileImagePath) {
+                $updateData['profile_pic'] = $profileImagePath;
+            }
 
+            $jobSeeker->update($updateData);
             return response()->json(['status' => 'success', 'jobSeeker' => $jobSeeker]);
         }
 
         return response()->json(['status' => 'error', 'message' => 'Job seeker not found'], 404);
+    }
+
+    public function getRelatedJobPosts(Request $request)
+    {
+
+        $jobSeeker = $request->user()->jobSeeker;
+        if ($jobSeeker) {
+            $industry = $jobSeeker->industry_id;
+            $jobPosts = JobPost::where('industry_id', $industry)->get();
+            return response()->json(['status' => 'success', 'jobPosts' => $jobPosts]);
+        }
+
+        return response()->json(['status' => 'error', 'message' => 'Job seeker not found'], 404);
+    }
+
+    public function applyForJob(Request $request)
+    {
+        $jobPostID = $request->job_post_id;
+        $jobSeeker = $request->user()->jobSeeker;
+        $jobPost = JobPost::find($jobPostID);
+        if ($jobPost) {
+            $jobPost->jobSeekers()->attach($jobSeeker);
+            return response()->json(['status' => 'success', 'message' => 'Applied successfully']);
+        }
     }
 }
