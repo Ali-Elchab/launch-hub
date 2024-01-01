ae84d52dac6ca1c5c2d5808469e8de3a5ba46b19
Ali-Elchab <alielchab01@gmail.com>
2024-01-01 23:14:05 +0200

    Fronend: Adjusted main for navigation

--- launchhub_frontend/lib/main.dart
+++ launchhub_frontend/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:launchhub_frontend/screens/signin.dart';
 import 'package:launchhub_frontend/screens/start_screen.dart';
 import 'package:google_fonts/google_fonts.dart';
 
