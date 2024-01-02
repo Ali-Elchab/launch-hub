c7df87a89c44b05209a2d6c48cfccb4a42a93764
Ali-Elchab <alielchab01@gmail.com>
2024-01-02 15:32:11 +0200

    Frontend: Created small_button widget

---
+++ launchhub_frontend/lib/widgets/small_button.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class SmallButton extends StatelessWidget {
+  const SmallButton(this.text, this.onPressed, {super.key});
+  final String text;
+  final void Function() onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 90,
+      child: TextButton(
+        onPressed: onPressed,
+        style: TextButton.styleFrom(
+          padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 15),
+          backgroundColor: const Color(0xFF326789),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              text,
+              style: Theme.of(context).textTheme.labelLarge!.copyWith(
+                  color: const Color.fromARGB(204, 255, 255, 255),
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16),
+            ),
+            const Icon(Icons.arrow_forward_ios, color: Colors.white, size: 16),
+          ],
+        ),
+      ),
+    );
+  }
+}
