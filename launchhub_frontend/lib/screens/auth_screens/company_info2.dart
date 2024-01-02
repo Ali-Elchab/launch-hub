15191300b674496b205a84b2e5736e3bdc3c7967
Ali-Elchab <alielchab01@gmail.com>
2024-01-02 23:25:07 +0200

    Frontend: Created company info 2 screen

---
+++ launchhub_frontend/lib/screens/auth_screens/company_info2.dart
@@ -0,0 +1,84 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:launchhub_frontend/widgets/auth_widgets/bottom_text.dart';
+import 'package:launchhub_frontend/widgets/auth_widgets/social_media_inputs.dart';
+import 'package:launchhub_frontend/widgets/custom_appbar.dart';
+import 'package:launchhub_frontend/widgets/input_field.dart';
+import 'package:launchhub_frontend/widgets/small_button.dart';
+import 'package:image_picker/image_picker.dart';
+
+class CompanyInfo2 extends StatefulWidget {
+  final XFile? selectedImage;
+
+  const CompanyInfo2({super.key, required this.selectedImage});
+
+  @override
+  State<CompanyInfo2> createState() => _CompanyInfo2State();
+}
+
+class _CompanyInfo2State extends State<CompanyInfo2> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset:
+          true, // Set to true to resize when keyboard appears
+      appBar: const CustomAppBar(title: 'Startup Profile'),
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        // Makes the body scrollable
+        child: Center(
+          child: SizedBox(
+            width: 280,
+            child: Column(
+              // Use Column without Expanded
+              children: <Widget>[
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8),
+                    child: Text(
+                      'Company Information',
+                      textAlign: TextAlign.left,
+                      style: Theme.of(context)
+                          .textTheme
+                          .displayMedium!
+                          .copyWith(fontWeight: FontWeight.w700, fontSize: 22),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 35),
+                if (widget.selectedImage != null)
+                  ClipOval(
+                    child: Image.file(
+                      File(widget.selectedImage!.path),
+                      width: 120, // Diameter of the circle
+                      height: 120, // Diameter of the circle
+                      fit: BoxFit
+                          .cover, // This ensures the image covers the circle crop
+                    ),
+                  ),
+                const SizedBox(height: 32),
+                const InputField(label: 'Business Address'),
+                const SizedBox(height: 16),
+                const InputField(label: 'Phone Number'),
+                const SizedBox(height: 16),
+                const InputField(label: 'Email Address'),
+                const SizedBox(height: 16),
+                const InputField(label: 'Website URL'),
+                const SizedBox(height: 16),
+                const SocialMediaLinksDropdown(),
+                const SizedBox(height: 150),
+                SmallButton('Next', () {
+                  // TODO: Implement the next button's onPressed function
+                }),
+                const SizedBox(height: 15),
+                const BottomText(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
