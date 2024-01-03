3336233358559f321c3630e503303d9ce58e784c
Ali-Elchab <alielchab01@gmail.com>
2024-01-03 16:31:30 +0200

    Frontend: Fixed auto size in small button

--- launchhub_frontend/lib/widgets/small_button.dart
+++ launchhub_frontend/lib/widgets/small_button.dart
@@ -7,28 +7,31 @@ class SmallButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: 90,
-      child: TextButton(
-        onPressed: onPressed,
-        style: TextButton.styleFrom(
-          padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 15),
-          backgroundColor: const Color(0xFF326789),
+    return Wrap(
+      children: [
+        TextButton(
+          onPressed: onPressed,
+          style: TextButton.styleFrom(
+            padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 15),
+            backgroundColor: const Color(0xFF326789),
+          ),
+          child: Row(
+            mainAxisSize: MainAxisSize.min, // Make Row take minimum space
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                text,
+                style: Theme.of(context).textTheme.labelLarge!.copyWith(
+                    color: const Color.fromARGB(204, 255, 255, 255),
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16),
+              ),
+              const Icon(Icons.arrow_forward_ios,
+                  color: Colors.white, size: 16),
+            ],
+          ),
         ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(
-              text,
-              style: Theme.of(context).textTheme.labelLarge!.copyWith(
-                  color: const Color.fromARGB(204, 255, 255, 255),
-                  fontWeight: FontWeight.bold,
-                  fontSize: 16),
-            ),
-            const Icon(Icons.arrow_forward_ios, color: Colors.white, size: 16),
-          ],
-        ),
-      ),
+      ],
     );
   }
 }
