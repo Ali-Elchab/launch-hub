4662c2e93dc323cd105d5713c86a15883e5d4600
Ali-Elchab <alielchab01@gmail.com>
2024-01-04 17:34:22 +0200

    Frontend: Added custom app bar to home

--- launchhub_frontend/lib/screens/startup_screens/home.dart
+++ launchhub_frontend/lib/screens/startup_screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:launchhub_frontend/widgets/profiles_shared/bottom_bar.dart';
 import 'package:launchhub_frontend/widgets/profiles_shared/header.dart';
 
 class Home extends StatefulWidget {
@@ -11,19 +12,40 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Color.fromARGB(255, 255, 255, 255),
-      resizeToAvoidBottomInset: false,
-      appBar: PreferredSize(
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+      appBar: const PreferredSize(
         preferredSize: Size.fromHeight(180),
         child: Header(
           title: 'Vast',
           showBackButton: false,
         ),
       ),
-      body: Center(
-        child: Text('Home'),
+      body: SingleChildScrollView(
+        physics: const ClampingScrollPhysics(),
+        child: Center(
+          child: SizedBox(
+            width: 280,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 25),
+                Column(
+                  children: [
+                    for (var i = 0; i < 3; i++)
+                      const Text(
+                        ' Welcome to Vast, your premier app for turning visions into reality. We are a community of entrepreneurs, investors, and mentors who are here to help you launch your startup. We provide a platform for you to connect with other entrepreneurs, investors, and mentors to help you launch your startup. We also provide a platform for you to connect with other entrepreneurs, investors, and mentors to help you launch your startup. We also provide a platform for you to connect with other entrepreneurs, investors, and mentors to help you launch your startup. We also provide a platform for you to connect with other entrepreneurs, investors, and mentors to help you launch your startup. We also provide a platform for you to connect with other entrepreneurs, investors, and mentors to help you launch your startup. We also provide a platform for you to connect with other entrepreneurs, investors, and mentors to help you launch your startup',
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
       ),
+      bottomNavigationBar: const CustomBottomNavigationBar(),
     );
   }
 }
