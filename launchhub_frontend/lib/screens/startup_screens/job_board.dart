39fc8dfed3f392e8c565b75a9167a7318bc36738
Ali-Elchab <alielchab01@gmail.com>
2024-01-06 23:13:48 +0200

    Fronend: Added job board screen

--- launchhub_frontend/lib/screens/startup_screens/job_board.dart
+++ launchhub_frontend/lib/screens/startup_screens/job_board.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:launchhub_frontend/data/mockData.dart';
+import 'package:launchhub_frontend/widgets/input_field.dart';
+import 'package:launchhub_frontend/widgets/profiles_shared/bottom_bar.dart';
+import 'package:launchhub_frontend/widgets/profiles_shared/header.dart';
+import 'package:launchhub_frontend/widgets/profiles_shared/job_post_card.dart';
+import 'package:launchhub_frontend/widgets/profiles_shared/search_input.dart';
 
 class JobBoard extends StatefulWidget {
   const JobBoard({super.key});
@@ -10,6 +16,47 @@ class JobBoard extends StatefulWidget {
 class _JobBoardState extends State<JobBoard> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+      appBar: const PreferredSize(
+        preferredSize: Size.fromHeight(180),
+        child: Header(
+          title: 'Job Board',
+          text:
+              'Cultivate your team and shape your company\'s future. Here in the Job board, effortlessly add, view, and manage your job listings, empowering you to connect with top talent seamlessly.',
+        ),
+      ),
+      body: Center(
+        child: SizedBox(
+          width: 300,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 45),
+              const Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: [Expanded(child: SearchInput())],
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: dummyJobPosts.length,
+                  itemBuilder: (context, index) => Column(
+                    children: [
+                      JobPostCard(
+                        jobPost: dummyJobPosts[index],
+                        onTap: () {},
+                      ),
+                      const SizedBox(height: 20),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: const CustomBottomNavigationBar(),
+    );
   }
 }
