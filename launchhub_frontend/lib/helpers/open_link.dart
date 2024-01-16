729645eb1281fdadf5358f68755780c13a8bffc6
Ali-Elchab <alielchab01@gmail.com>
2024-01-16 17:19:48 +0200

    Frontend: Adjusted open link and download files helpers

--- launchhub_frontend/lib/helpers/open_link.dart
+++ launchhub_frontend/lib/helpers/open_link.dart
@@ -3,13 +3,11 @@ import 'package:url_launcher/url_launcher.dart';
 import 'package:flutter_file_downloader/flutter_file_downloader.dart';
 // import 'package:path_provider/path_provider.dart';
 import 'package:open_file/open_file.dart';
-
-// import 'package:dio/dio.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 openLink(context, String urlPath) async {
   final url = urlPath;
   if (!await launchUrl(Uri.parse(url))) {
-    // ignore: use_build_context_synchronously
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
         content: Text('Could not open  $url'),
@@ -20,25 +18,23 @@ openLink(context, String urlPath) async {
   }
 }
 
-// ignore: non_constant_identifier_names
-downloadFile(
-    {required BuildContext context, // Add the BuildContext parameter
-    String? url,
-    String? name}) async {
+downloadFile({required BuildContext context, String? url, String? name}) async {
+  await requestStoragePermission();
+
   FileDownloader.downloadFile(
       url: url!,
       name: name,
-      onProgress: (String? fileName, double? progress) {
-        // Optionally, update the UI to show download progress
-      },
+      onProgress: (String? fileName, double? progress) {},
       onDownloadCompleted: (String path) {
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(
-            content: Text('Download completed! Tap to open the file.'),
+            content: const Text('Download completed! Tap to open the file.'),
             action: SnackBarAction(
               label: 'Open',
               onPressed: () async {
-                await OpenFile.open(path, type: 'csv/pdf');
+                // Determine the file type based on the file's extension
+                String fileType = getFileTypeFromExtension(path);
+                await OpenFile.open(path, type: fileType);
               },
             ),
           ),
@@ -51,4 +47,30 @@ downloadFile(
           ),
         );
       });
+}
+
+Future<void> requestStoragePermission() async {
+  var status = await Permission.storage.status;
+  if (!status.isGranted) {
+    await Permission.storage.request();
+  }
+}
+
+String getFileTypeFromExtension(String filePath) {
+  // Extract the file extension from the file path
+  String extension = filePath.split('.').last.toLowerCase();
+
+  // Map file extensions to MIME types or types recognized by OpenFile
+  Map<String, String> extensionToType = {
+    'pdf': 'application/pdf',
+    'doc': 'application/msword',
+    'docx':
+        'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+    'xls': 'application/vnd.ms-excel',
+    'xlsx': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+    // Add more extensions and corresponding types as needed
+  };
+
+  // Return the appropriate type for the given extension, or a default type
+  return extensionToType[extension] ?? 'application/octet-stream';
 }
