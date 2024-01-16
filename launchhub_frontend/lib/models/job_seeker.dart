fa83a3e40b8b9e2842e0c15dbd0f7c3210dcf755
Ali-Elchab <alielchab01@gmail.com>
2024-01-16 13:17:24 +0200

    Frontend: Fixed job seeker model

--- launchhub_frontend/lib/models/job_seeker.dart
+++ launchhub_frontend/lib/models/job_seeker.dart
@@ -1,8 +1,4 @@
-import 'package:launchhub_frontend/models/industry.dart';
-import 'package:launchhub_frontend/models/niche.dart';
-
 import 'job_post.dart';
-import 'social_media_link.dart';
 import 'education.dart';
 import 'experience.dart';
 import 'certification.dart';
@@ -10,7 +6,7 @@ import 'hobby.dart';
 import 'skill.dart';
 
 class JobSeeker {
-  final int id;
+  int? id;
   final String firstName;
   final String lastName;
   final String? profilePic;
@@ -19,20 +15,19 @@ class JobSeeker {
   final String address;
   final String bio;
   final String? resume;
-  final bool isAvailable;
-  final Industry industryId;
-  final int userId;
-  final Niche specializationId;
-  final List socialMediaLinks;
-  final List<Education> educations;
-  final List<Experience> experiences;
-  final List<Certification> certifications;
-  final List<Hobby> hobbies;
-  final List<Skill> skills;
-  final List<JobPost> jobPosts;
-  final String proffesionalSummary;
+  bool? isAvailable;
+  final int industryId;
+  int? userId;
+  final int specializationId;
+  final List? socialMediaLinks;
+  final List<Education>? educations;
+  final List<Experience>? experiences;
+  final List<Certification>? certifications;
+  final List<Hobby>? hobbies;
+  final List<Skill>? skills;
+  final List<JobPost>? jobPosts;
   JobSeeker({
-    required this.id,
+    this.id,
     required this.firstName,
     required this.lastName,
     this.profilePic,
@@ -40,24 +35,23 @@ class JobSeeker {
     required this.phone,
     required this.address,
     required this.bio,
-    required this.proffesionalSummary,
     this.resume,
-    required this.isAvailable,
+    this.isAvailable,
     required this.industryId,
-    required this.userId,
+    this.userId,
     required this.specializationId,
-    required this.socialMediaLinks,
-    required this.educations,
-    required this.experiences,
-    required this.certifications,
-    required this.hobbies,
-    required this.skills,
-    required this.jobPosts,
+    this.socialMediaLinks,
+    this.educations,
+    this.experiences,
+    this.certifications,
+    this.hobbies,
+    this.skills,
+    this.jobPosts,
   });
 
   factory JobSeeker.fromJson(Map<String, dynamic> json) {
     return JobSeeker(
-      id: json['id'] as int,
+      id: json['id'],
       firstName: json['first_name'] as String,
       lastName: json['last_name'] as String,
       profilePic: json['profile_pic'] as String?,
@@ -65,36 +59,49 @@ class JobSeeker {
       phone: json['phone'] as String,
       address: json['address'] as String,
       bio: json['bio'] as String,
-      proffesionalSummary: json['proffesional_summary'] as String,
       resume: json['resume'] as String?,
-      isAvailable: (json['is_available'] as int) == 1,
-      userId: json['user_id'] as int,
-      industryId: (json['industry_id'] != null)
-          ? Industry.fromJson(json['industry_id'])
-          : Industry(id: 0, name: 'None'),
-      specializationId: (json['specialization_id'] != null)
-          ? Niche.fromJson(json['specialization_id'])
-          : Niche(id: 0, name: 'None', industryId: 0),
-      socialMediaLinks: (json['socialMediaLinks'] as List)
-          .map((i) => SocialMediaLink.fromJson(i))
-          .toList(),
-      educations: (json['educations'] as List)
-          .map((i) => Education.fromJson(i))
-          .toList(),
-      experiences: (json['experiences'] as List)
-          .map((i) => Experience.fromJson(i))
-          .toList(),
-      certifications: (json['certifications'] as List)
-          .map((i) => Certification.fromJson(i))
-          .toList(),
-      hobbies: (json['hobbies'] as List).map((i) => Hobby.fromJson(i)).toList(),
-      skills: (json['skills'] as List).map((i) => Skill.fromJson(i)).toList(),
-      jobPosts:
-          (json['job_posts'] as List).map((i) => JobPost.fromJson(i)).toList(),
+      isAvailable: json['is_available'],
+      userId: json['user_id'] as int?,
+      industryId: json['industry_id'] as int,
+      specializationId: json['industry_id'] as int,
+      // socialMediaLinks: (json['socialMediaLinks'] as List)
+      //     .map((i) => SocialMediaLink.fromJson(i))
+      //     .toList(),
+      // educations: (json['educations'] as List)
+      //     .map((i) => Education.fromJson(i))
+      //     .toList(),
+      // experiences: (json['experiences'] as List)
+      //     .map((i) => Experience.fromJson(i))
+      //     .toList(),
+      // certifications: (json['certifications'] as List)
+      //     .map((i) => Certification.fromJson(i))
+      //     .toList(),
+      // hobbies: (json['hobbies'] as List).map((i) => Hobby.fromJson(i)).toList(),
+      // skills: (json['skills'] as List).map((i) => Skill.fromJson(i)).toList(),
+      // jobPosts:
+      //     (json['job_posts'] as List).map((i) => JobPost.fromJson(i)).toList(),
     );
   }
 
-  Map<String, dynamic> toJson() {
+  static Map<String, dynamic> toJson(
+      {firstName,
+      lastName,
+      profilePic,
+      dob,
+      phone,
+      address,
+      bio,
+      resume,
+      industryId,
+      userId,
+      specializationId,
+      socialMediaLinks,
+      educations,
+      experiences,
+      certifications,
+      hobbies,
+      skills,
+      jobPosts}) {
     final Map<String, dynamic> data = <String, dynamic>{};
     data['first_name'] = firstName;
     data['last_name'] = lastName;
@@ -103,19 +110,16 @@ class JobSeeker {
     data['phone'] = phone;
     data['address'] = address;
     data['bio'] = bio;
-    data['proffesional_summary'] = proffesionalSummary;
     data['resume'] = resume;
-    data['is_available'] = isAvailable;
     data['industry_id'] = industryId;
     data['user_id'] = userId;
     data['specialization_id'] = specializationId;
-    data['socialMediaLinks'] = socialMediaLinks;
+    data['social_media_links'] = socialMediaLinks;
     data['educations'] = educations;
     data['experiences'] = experiences;
     data['certifications'] = certifications;
     data['hobbies'] = hobbies;
     data['skills'] = skills;
-    data['job_posts'] = jobPosts;
     return data;
   }
 }
