f027cd422e134dc41c935665ef075b03b7bf5c9a
Ali-Elchab <alielchab01@gmail.com>
2024-01-21 22:36:43 +0200

    Frontend: Adjusted applicants list

--- launchhub_frontend/lib/widgets/startup/applicants_list.dart
+++ launchhub_frontend/lib/widgets/startup/applicants_list.dart
@@ -5,9 +5,11 @@ import 'package:launchhub_frontend/models/job_seeker.dart';
 import 'package:launchhub_frontend/widgets/profiles_shared/job_seeker_card.dart';
 
 class ApplicantsList extends StatelessWidget {
-  const ApplicantsList({super.key, required this.jobSeekers, onTap});
+  const ApplicantsList(
+      {super.key, required this.jobSeekers, onTap, this.onDismissed});
 
   final List<JobSeeker> jobSeekers;
+  final Function? onDismissed;
 
   @override
   Widget build(BuildContext context) {
@@ -17,27 +19,27 @@ class ApplicantsList extends StatelessWidget {
         children: [
           Dismissible(
             key: ValueKey(jobSeekers[index].id),
-            background: Container(
-              color: Colors.red,
+            secondaryBackground: Container(
+              color: const Color.fromARGB(255, 165, 11, 0),
               alignment: Alignment.centerRight,
               padding: const EdgeInsets.only(right: 20),
               margin: const EdgeInsets.symmetric(
                 horizontal: 15,
-                vertical: 4,
+                vertical: 12,
               ),
               child: const Icon(
-                Icons.delete,
+                Icons.thumb_down_rounded,
                 color: Colors.white,
                 size: 40,
               ),
             ),
-            secondaryBackground: Container(
+            background: Container(
               color: Theme.of(context).primaryColor,
-              alignment: Alignment.centerRight,
-              padding: const EdgeInsets.only(right: 20),
+              alignment: Alignment.centerLeft,
+              padding: const EdgeInsets.only(left: 20),
               margin: const EdgeInsets.symmetric(
                 horizontal: 15,
-                vertical: 4,
+                vertical: 12,
               ),
               child: const Icon(
                 Icons.contact_page_outlined,
@@ -52,7 +54,7 @@ class ApplicantsList extends StatelessWidget {
                   builder: (ctx) => AlertDialog(
                     title: const Text('Are you sure?'),
                     content: const Text(
-                      'Do you want to remove the item from the list?',
+                      'Do you want to reject application? Tap the card and view the applicant\'s profile to make sure.',
                     ),
                     actions: [
                       TextButton(
@@ -70,15 +72,54 @@ class ApplicantsList extends StatelessWidget {
                     ],
                   ),
                 );
+              } else {
+                return await showDialog(
+                  context: context,
+                  builder: (ctx) => AlertDialog(
+                    title: const Text('Contact Applicant Now!'),
+                    content: SizedBox(
+                      height: 130,
+                      child: Column(
+                        children: [
+                          TextButton.icon(
+                            icon: Icon(
+                              Icons.email_outlined,
+                              color: Theme.of(context).primaryColor,
+                            ),
+                            label: const Text('Contact via Email'),
+                            onPressed: () async {
+                              String email = jobSeekers[index].email!;
+                              String mailtoUrl = "mailto:$email";
+                              openLink(context, mailtoUrl);
+                              navigatorKey.currentState!.pop();
+                            },
+                          ),
+                          const SizedBox(height: 10),
+                          TextButton.icon(
+                            icon: Icon(
+                              Icons.phone,
+                              color: Theme.of(context).primaryColor,
+                            ),
+                            label: const Text('Contact via Phone'),
+                            onPressed: () async {
+                              String phone = jobSeekers[index].phone;
+                              String phoneCallUrl = "tel:$phone";
+                              openLink(context, phoneCallUrl);
+                              navigatorKey.currentState!.pop();
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
               }
-              return null;
             },
-            onDismissed: (direction) {
+            onDismissed: (direction) async {
               if (direction == DismissDirection.endToStart) {
-                // Provider.of<Cart>(context, listen: false)
-                //     .removeItem(product.id);
+                onDismissed!(jobSeekers[index].id, 'rejected', index);
               } else if (direction == DismissDirection.startToEnd) {
-                showDialog(
+                await showDialog(
                   context: context,
                   builder: (ctx) => AlertDialog(
                     title: const Text('Contact Applicant Now!'),
