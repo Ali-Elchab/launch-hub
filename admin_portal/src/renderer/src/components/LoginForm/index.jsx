458c3567cd7c5b3cc224b90a8d0ec9a020989f81
Ali-Elchab <alielchab01@gmail.com>
2024-01-23 20:13:22 +0200

    Dashboard: Implemented login function

--- admin_portal/src/renderer/src/components/LoginForm/index.jsx
+++ admin_portal/src/renderer/src/components/LoginForm/index.jsx
@@ -3,34 +3,37 @@ import "./styles.css";
 import InputField from "../InputField";
 import { Link, useNavigate } from "react-router-dom";
 import Button from "../Button";
-// import { setUser } from "../../core/redux/user/userSlice";
-// import { useDispatch } from "react-redux";
-// import { requestData } from "../../core/axios";
-// import axios from "axios";
+import { setUser } from "../../core/redux/user/userSlice";
+import { useDispatch } from "react-redux";
+import { requestData } from "../../core/axios";
+
 const LoginForm = () => {
-  // const navigate = useNavigate();
-  // const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
   const [values, setValues] = useState({
     email: "",
     password: "",
   });
   const HandleOnInputChange = (e) => {
     setValues({ ...values, [e.target.name]: e.target.value });
-    console.log(values);
   };
-  // const handleLogin = async () => {
-  //   try {
-  //     const res = await requestData("login", "post", values);
-  //     if (res.status == "success") {
-  //       localStorage.setItem("token", `Bearer ${res.authorisation.token}`);
-  //       console.log(res.user);
-  //       dispatch(setUser(res.user));
-  //       navigate("/drivers");
-  //     }
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // };
+  const handleLogin = async () => {
+    try {
+      const res = await requestData("login", "post", values);
+      if (res.status == "success") {
+        localStorage.setItem("token", `Bearer ${res.authorisation.token}`);
+        console.log(res.user);
+        if (res.user["user_type_id"] == 3) {
+          dispatch(setUser(res.user));
+          navigate("/statistics");
+        } else {
+          return alert("You are not an admin");
+        }
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <form className="login-form">
       <h2 className="form-title">Enter Admin Credentials</h2>
@@ -58,7 +61,7 @@ const LoginForm = () => {
           />
         </div>
         <div className="btn-wrapper">
-          <Button text={"Login"} type={"submit"} />
+          <Button text={"Login"} type={"submit"} handleOnClick={handleLogin} />
         </div>
       </div>
     </form>
