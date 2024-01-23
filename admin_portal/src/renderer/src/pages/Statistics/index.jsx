966cea50d943c629c5598779d8b5f8f5d604e6c2
Ali-Elchab <alielchab01@gmail.com>
2024-01-24 00:01:30 +0200

    Dashboard: Added statistics page

--- admin_portal/src/renderer/src/pages/Statistics/index.jsx
+++ admin_portal/src/renderer/src/pages/Statistics/index.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import "./styles.css";
+import TotalRegistrations from "../../components/Dashboard/TotalRegistrations";
 
 const Statistics = () => {
-  return <div className="">Statissssssssssssssssssssssssssssssssssssssssstics</div>;
+  return (
+    <div className="content-container ">
+      <TotalRegistrations />
+      <div>ssss</div>
+    </div>
+  );
 };
 
-export default Statistics
+export default Statistics;
