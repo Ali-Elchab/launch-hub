dd6861880f91e39c16e73008a5207c2d620d6350
Ali-Elchab <alielchab01@gmail.com>
2024-01-23 20:05:52 +0200

    Dashboard: Initialized App.jsx routes

--- admin_portal/src/renderer/src/App.jsx
+++ admin_portal/src/renderer/src/App.jsx
@@ -1,5 +1,20 @@
+import "./styles/index.css";
+import Login from "./pages/Login";
+import { Route, Routes } from "react-router";
+import Statistics from "./pages/Statistics";
+
 function App() {
-  return <div>Hello World </div>
+  return (
+    <div className="page flex row">
+      <div className="main">
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/statistics" element={<Statistics />} />
+        </Routes>
+      </div>
+      //{" "}
+    </div>
+  );
 }
 
-export default App
+export default App;
