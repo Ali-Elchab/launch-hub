895358a193fd04bc50bfb124d1d4982fee690b44
Ali-Elchab <alielchab01@gmail.com>
2024-01-24 01:20:48 +0200

    Dashboard: Added pie chart to statistics page

--- admin_portal/src/renderer/src/pages/Statistics/index.jsx
+++ admin_portal/src/renderer/src/pages/Statistics/index.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import "./styles.css";
 import TotalRegistrations from "../../components/Dashboard/TotalRegistrations";
+import MyResponsivePie from "../../components/Dashboard/UserDemographics";
 
 const Statistics = () => {
   return (
     <div className="content-container ">
       <TotalRegistrations />
-      <div>ssss</div>
+      <div className="flex row " style={{ height: 500, gap: 30 }}>
+        <div className="userStats" style={{ width: "40%" }}>
+          <h2 style={{ color: "#326789" }}>Number of Users</h2>
+          <MyResponsivePie jobSeekers={13} startups={50} />
+        </div>
+      </div>
     </div>
   );
 };
