bc709c8d8b79228ebbd78718c9ac1e44a25bc2af
Ali-Elchab <alielchab01@gmail.com>
2024-01-24 07:12:44 +0200

    Dashboard: Adjusted total registration component

--- admin_portal/src/renderer/src/components/Dashboard/TotalRegistrations/index.jsx
+++ admin_portal/src/renderer/src/components/Dashboard/TotalRegistrations/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./styles.css";
 import { FiArrowUpCircle } from "react-icons/fi";
 
-const TotalRegistrations = () => {
+const TotalRegistrations = ({ totalRegisteredUsers }) => {
   return (
     <div className="total-registrations flex center ">
       <div className="total-registrations_this_month flex row ">
@@ -12,7 +12,7 @@ const TotalRegistrations = () => {
         <div className="flex column ">
           <h2>Total Registrations</h2>
           <h6>This Month</h6>
-          <h2 style={{ marginTop: "20px" }}>1958</h2>
+          <h2 style={{ marginTop: "20px" }}>{totalRegisteredUsers}</h2>
         </div>
       </div>
       <div className="total-registrations_this_month flex row ">
--- admin_portal/src/renderer/src/pages/Statistics/index.jsx
+++ admin_portal/src/renderer/src/pages/Statistics/index.jsx
@@ -1,13 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { requestData } from "../../core/axios";
+
 import "./styles.css";
 import TotalRegistrations from "../../components/Dashboard/TotalRegistrations";
 import MyResponsivePie from "../../components/Dashboard/UserDemographics";
 import MyResponsiveBar from "../../components/Dashboard/ApplicationsStats";
 
 const Statistics = () => {
-  return (
+  const [values, setValues] = useState({
+    startups: 0,
+    jobSeekers: 0,
+    pendingApplications: 0,
+    rejectedApplications: 0,
+    totalRegisteredUsers: 0,
+    leadingIndustry: "",
+  });
+  const [isLoading, setIsLoading] = useState(true);
+
+  const getStatistics = async () => {
+    const token = localStorage.getItem("token");
+    const headers = {
+      Authorization: token,
+    };
+    try {
+      const res = await requestData(
+        "admin/statistics",
+        "get",
+        null,
+        headers,
+      ).then((res) => {
+        setIsLoading(false);
+
+        console.log(values.totalRegisteredUsers);
+        setValues(res);
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  useEffect(() => {
+    getStatistics();
+  }, []);
+  !isLoading ? (
     <div className="content-container ">
-      <TotalRegistrations />
+      <TotalRegistrations totalRegisteredUsers={values.totalRegisteredUsers} />
       <div className="flex row " style={{ gap: 30 }}>
         <div className="userStats" style={{ height: 500, width: "40%" }}>
           <h2 style={{ color: "#326789" }}>Number of Users</h2>
@@ -19,7 +55,7 @@ const Statistics = () => {
         </div>
       </div>
     </div>
-  );
+  ) : null;
 };
 
 export default Statistics;
