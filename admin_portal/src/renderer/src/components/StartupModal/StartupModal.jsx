f3c2b9ff59dcb3a5711b2f6b82427c9f5d53ae1f
Ali-Elchab <alielchab01@gmail.com>
2024-01-25 08:05:09 +0200

    Dashbaord: Added startup details to modal

--- admin_portal/src/renderer/src/components/StartupModal/StartupModal.jsx
+++ admin_portal/src/renderer/src/components/StartupModal/StartupModal.jsx
@@ -1,13 +1,54 @@
 import React from "react";
 import "./styles.css";
+import { BaseURL } from "../../core/helpers/BaseImageURL";
 
-export const StartupModal = ({ startup, onClose }) => {
+export const StartupModal = ({ startup, onClose, onDelete }) => {
   return (
     <div className="modal-backdrop">
       <div className="modal-content">
-        <h2>{startup["company_name"]}</h2>
-        {/* other details */}
-        <button onClick={onClose}>Close</button>
+        <div className="flex top-row space-between">
+          <div className="flex center">
+            <img
+              src={`${BaseURL}assets/images/profile_pics/${startup["logo_url"]}`}
+              alt="Network Image"
+              className="profile-pic"
+            />
+            <h2>{startup["company_name"]}</h2>
+          </div>
+          <button className="exit-button" onClick={onClose}>
+            <span>&times;</span>
+          </button>
+        </div>
+        <div className="flex column about">
+          <h3>About</h3>
+          <p>{startup["company_description"]}</p>
+          <p>
+            <strong>Industry:</strong> {startup["industry_id"]}
+          </p>
+          <p>
+            <strong>Startup:</strong> {startup["company_name"]}
+          </p>
+          <p>
+            <strong>Phone:</strong> {startup["company_phone"]}
+          </p>
+          <p>
+            <strong>Registration Num:</strong> {startup["registration_number"]}
+          </p>
+          <p>
+            <strong>Founding Date:</strong> {startup["founding_date"]}
+          </p>
+          <p>
+            <strong>Address:</strong> {startup["company_address"]}
+          </p>
+          <p>
+            <strong>Website:</strong> {startup["website_url"]}
+          </p>
+        </div>
+        <div className="delete">
+          <button onClick={onDelete} className="delete-button">
+            Delete Account
+          </button>
+        </div>
       </div>
     </div>
   );
