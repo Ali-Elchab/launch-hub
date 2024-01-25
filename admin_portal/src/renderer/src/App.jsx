edb700bdf6faa7f6c5f49417bdd84a8b1e5a933c
Ali-Elchab <alielchab01@gmail.com>
2024-01-25 02:02:23 +0200

    Dashboard: Added user management to routes

--- admin_portal/src/renderer/src/App.jsx
+++ admin_portal/src/renderer/src/App.jsx
@@ -3,6 +3,7 @@ import Login from "./pages/Login";
 import { Route, Routes } from "react-router";
 import Statistics from "./pages/Statistics";
 import { Sidebar } from "./components/Sidebar";
+import UserManagement from "./pages/UserManagement/UserManagement";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/statistics" element={<Statistics />} />
+          <Route path="/user-management" element={<UserManagement />} />
         </Routes>
       </div>
     </div>
