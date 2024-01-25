f20734c0afbba2127fa2c13cd213780ba166058a
Ali-Elchab <alielchab01@gmail.com>
2024-01-25 16:13:50 +0200

    Dashboard: Added profile and content to routes

--- admin_portal/src/renderer/src/App.jsx
+++ admin_portal/src/renderer/src/App.jsx
@@ -5,6 +5,8 @@ import Statistics from "./pages/Statistics";
 import { Sidebar } from "./components/Sidebar";
 import UserManagement from "./pages/UserManagement/UserManagement";
 import JobPostManagement from "./pages/JobPostManagement/JobPostManagement";
+import { AdminProfile } from "./pages/Profile/AdminProfile";
+import ContentManagement from "./pages/ContentManagement";
 
 function App() {
   return (
@@ -16,6 +18,8 @@ function App() {
           <Route path="/statistics" element={<Statistics />} />
           <Route path="/user-management" element={<UserManagement />} />
           <Route path="/job-posts" element={<JobPostManagement />} />
+          <Route path="/profile" element={<AdminProfile />} />
+          <Route path="/content" element={<ContentManagement />} />
         </Routes>
       </div>
     </div>
