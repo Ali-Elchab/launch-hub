71cfbe4929c8715458fca763e7f08fe957711254
Ali-Elchab <alielchab01@gmail.com>
2024-01-25 08:41:59 +0200

    Dashboard: Added refresh token function

--- admin_portal/src/renderer/src/core/axios/index.js
+++ admin_portal/src/renderer/src/core/axios/index.js
@@ -18,3 +18,23 @@ export const requestData = async (route, method, data, headers = {}) =>
     .then((res) => {
       return res.data;
     });
+
+axios.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      try {
+        const refreshToken = localStorage.getItem("refreshToken");
+        const response = await axios.post("refresh", { refreshToken });
+        const { accessToken } = response.data.authorisation.token;
+        localStorage.setItem("accessToken", accessToken);
+        axios.defaults.headers.common["Authorization"] =
+          `Bearer ${accessToken}`;
+        return axios(originalRequest);
+      } catch (refreshError) {}
+    }
+    return Promise.reject(error);
+  },
+);
